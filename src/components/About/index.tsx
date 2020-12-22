55206c181fef44ad5d70f764eac2c9641b526f7f
EvgeniyGuzenkov <jack199479@gmail.com>
2020-12-22 12:36:45 +0300

    feat: animately display an about-me info by scrolling a viewport

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,34 +1,38 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import Title from '../../common/Title';
 import s from './About.module.css';
 import { useScroll, getEndpointHandler } from '../../common/hooks/use-scroll';
 
 const About = <E extends HTMLElement>({ viewport }: { viewport: E }) => {
-  const ref = useRef<HTMLParagraphElement>(null); 
+  const [isShow, setShowState] = useState<boolean>(false);
+  const descriptionRef = useRef<HTMLDivElement>(null);
 
   useScroll(
     viewport,
-    getEndpointHandler(ref, {
-      endPoint: 1,
-      cb: () => console.log('CALLBACK LOWER'),
-      upperCb: () => console.log('Callback upper'),
+    getEndpointHandler(descriptionRef, {
+      endPoint: 0.3,
+      cb: () => setShowState(true),
     }),
   );
 
   return (
     <section className={s.wrapper}>
-      <div className={s.container}>
+      <div className={s.container} ref={descriptionRef}>
         <Title>About</Title>
-        <p className={s.details}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est laudantium quaerat sed
-          dolores distinctio ducimus molestias quidem totam, vitae corrupti fugit porro voluptatem
-          repellendus dolore tempore, quasi nesciunt, ullam accusantium.
-        </p>
-        <p className={s.details} ref={ref}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est laudantium quaerat sed
-          dolores distinctio ducimus molestias quidem totam, vitae corrupti fugit porro voluptatem
-          repellendus dolore tempore, quasi nesciunt, ullam accusantium.
-        </p>
+        {isShow && (
+          <div className={s.description}>
+            <p className={s.details}>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est laudantium quaerat sed
+              dolores distinctio ducimus molestias quidem totam, vitae corrupti fugit porro
+              voluptatem repellendus dolore tempore, quasi nesciunt, ullam accusantium.
+            </p>
+            <p className={s.details}>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est laudantium quaerat sed
+              dolores distinctio ducimus molestias quidem totam, vitae corrupti fugit porro
+              voluptatem repellendus dolore tempore, quasi nesciunt, ullam accusantium.
+            </p>
+          </div>
+        )}
       </div>
     </section>
   );
