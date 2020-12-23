b0ce111fccce7be7898f87fb264e509d5bcf29ee
EvgeniyGuzenkov <jack199479@gmail.com>
2020-12-23 22:24:19 +0300

    feat: add Section component

--- src/common/Title/index.tsx
+++ src/common/Title/index.tsx
@@ -3,9 +3,9 @@ import classnames from 'classnames';
 import s from './Title.module.css';
 
 const Title: React.FC<SpreadingProps<HTMLHeadingElement>> = ({ children, className }) => {
-  const titleCssClass = classnames(s.title, { [className as string]: !!className });
+  const cssClass = classnames(s.title, { [className as string]: !!className });
 
-  return <h2 className={titleCssClass}>{children}</h2>;
+  return <h2 className={cssClass}>{children}</h2>;
 };
 
 export default Title;
