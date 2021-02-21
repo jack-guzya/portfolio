6d3491ea2c8bbc54b6d33b3ec13ae833e3d6c46b
Evgeniy Guzenkov <jack199479@gmail.com>
2021-02-21 15:47:37 +0300

    feat: add init to useScroll

--- src/hooks/use-scroll/use-scroll.ts
+++ src/hooks/use-scroll/use-scroll.ts
@@ -1,11 +1,13 @@
 import { useEffect } from 'react';
 
-const useScroll = (handleScroll: EventListener) => {
+const useScroll = (handleScroll: EventListener, { isInit } = { isInit: true }) => {
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
 
+    isInit && window.dispatchEvent(new Event('scroll'));
+
     return () => window.removeEventListener('scroll', handleScroll);
-  });
+  }, [handleScroll, isInit]);
 };
 
 export default useScroll;
